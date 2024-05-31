01eb60fe96b939195b073e8f669cdc4c83de12c0
nangelv <12814183+nangelv@users.noreply.github.com>
2024-05-31 17:21:53 +0200

    Extract URL path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,18 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
     System.out.println("Logs from your program will appear here!");
-    
+
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
-    
+
     try {
       serverSocket = new ServerSocket(4221);
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
@@ -17,7 +20,16 @@ public class Main {
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
-      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      try (var reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+        var path = reader.readLine().split(" ")[1];
+        System.out.println("Path is " + path);
+        if (path.equals("/")) {
+          clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        } else {
+          clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+        }
+      }
+
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
