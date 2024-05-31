eb5fb8263040d293d746bdc9d34983c1bef3ebf7
nangelv <12814183+nangelv@users.noreply.github.com>
2024-05-31 22:05:17 +0200

    Get a file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
+
+    public static String SERVER_DIRECTORY;
+
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible
-        // when running tests.
-        System.out.println("Logs from your program will appear here!");
+        if (args.length > 1 && args[0].equals("--directory")) {
+            SERVER_DIRECTORY = args[1];
+        }
+        var port = 4221;
+        System.out.printf("Server operational on port %d 🚜%n", port);
 
-        ServerSocket serverSocket = null;
-
-        try {
-            serverSocket = new ServerSocket(4221);
+        try(var serverSocket = new ServerSocket(port)) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
@@ -29,35 +33,69 @@ public class Main {
 
     private static void handleHttpConnection(Socket clientSocket) {
         try {
-            System.out.println("accepted new connection");
-            var templateResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s";
+            System.out.println("Accepted new connection from " + clientSocket.getRemoteSocketAddress());
             try (var reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
                 var path = reader.readLine().split(" ")[1];
                 if (path.equals("/")) {
-                    clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    okResponse(clientSocket);
                 } else if (path.startsWith("/echo/")) {
                     var echoArgument = path.substring("/echo/".length());
-                    var response = String.format(templateResponse, echoArgument.length(), echoArgument);
-                    clientSocket.getOutputStream().write(response.getBytes());
+                    okResponse(clientSocket, echoArgument);
                 } else if (path.equals("/user-agent")) {
-                    // Read headers until user agent is found
-                    String userAgent = null;
-                    String header = reader.readLine();
-                    while (header != null) {
-                        if (header.startsWith("User-Agent:")) {
-                            userAgent = header.substring("User-Agent:".length()).trim();
-                            break;
-                        }
-                        header = reader.readLine();
+                    String userAgent = readHeader(reader, "User-Agent");
+                    okResponse(clientSocket, userAgent);
+                } else if(path.startsWith("/files/")) {
+                    var fileName = path.substring("/files/".length());
+                    var filePath = Path.of(SERVER_DIRECTORY, fileName);
+                    if (Files.exists(filePath)) {
+                        var fileContent = Files.readString(filePath);
+                        okResponse(clientSocket, fileContent, "application/octet-stream");
+                    } else {
+                        notFoundResponse(clientSocket);
                     }
-                    var response = String.format(templateResponse, userAgent.length(), userAgent);
-                    clientSocket.getOutputStream().write(response.getBytes());
                 } else {
-                    clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+                    notFoundResponse(clientSocket);
                 }
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
+    }
+
+    private static String readHeader(BufferedReader reader, String headerToFind) throws IOException {
+        if (!headerToFind.endsWith(":")) {
+            headerToFind += ":";
+        }
+        String headerLine = reader.readLine();
+        String headerValue = null;
+        while (headerLine != null) {
+            // Implementation is case-sensitive, HTTP headers are case-insensitive by specification
+            if (headerLine.startsWith(headerToFind)) {
+                headerValue = headerLine.substring(headerToFind.length()).trim();
+                break;
+            }
+            headerLine = reader.readLine();
+        }
+        return headerValue;
+    }
+
+    private static void notFoundResponse(Socket clientSocket) throws IOException {
+        clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+    }
+
+    private static void okResponse(Socket clientSocket, String body, String contentType) throws IOException {
+        var okResponse = "HTTP/1.1 200 OK\r\n\r\n";
+        if (body != null) {
+            okResponse += "Content-Type: %s\r\nContent-Length: %d\r\n\r\n%s".formatted(contentType, body.length(), body);
+        }
+        clientSocket.getOutputStream().write(okResponse.getBytes());
+    }
+
+    private static void okResponse(Socket clientSocket, String body) throws IOException {
+        okResponse(clientSocket, body, "text/plain");
+    }
+
+    private static void okResponse(Socket clientSocket) throws IOException {
+        okResponse(clientSocket, null);
     }
 }
