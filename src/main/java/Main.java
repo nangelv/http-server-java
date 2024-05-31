1302fb7ff6018903013a555be76e3d0e39f7c154
nangelv <12814183+nangelv@users.noreply.github.com>
2024-05-31 19:36:03 +0200

    Respond with body (2)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,10 +22,16 @@ public class Main {
       System.out.println("accepted new connection");
       try (var reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
         var path = reader.readLine().split(" ")[1];
-        var echoArgument = path.substring(path.lastIndexOf("/") + 1);
-        var templateResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s";
-        var response = String.format(templateResponse, echoArgument.length(), echoArgument);
-        clientSocket.getOutputStream().write(response.getBytes()); 
+        if (path.equals("/")) {
+          clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        } else if (path.startsWith("/echo/")) {
+          var echoArgument = path.substring("/echo/".length());
+          var templateResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s";
+          var response = String.format(templateResponse, echoArgument.length(), echoArgument);
+          clientSocket.getOutputStream().write(response.getBytes());
+        } else {
+          clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+        }
       }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
